a17e1abbaf6290f19944e17d0990dbe8dc568bfd
Chester Mikhail De Guzman <140163654+chesterdeguzman@users.noreply.github.com>
2026-06-14 00:02:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+function toggleReadMore() {
+  const content = document.getElementById("more-about");
+  const button = document.querySelector(".read-more-btn");
+
+  content.classList.toggle("show");
+
+  if (content.classList.contains("show")) {
+    button.innerText = "Show Less ♡";
+  } else {
+    button.innerText = "Read More ♡";
+  }
 }
